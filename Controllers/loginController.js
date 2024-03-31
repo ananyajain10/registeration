973cf7d6f28fc2c2781e12dfdeec57874941d2d9
ananyajain10 <jainananya355@gmail.com>
2024-03-31 17:30:49 +0530

    Revert "res Message on send otp"
    This reverts commit 60db59208bdcdd9f15cb87b263afdb91001a1288.
    
    res Message on send otp

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -70,13 +70,7 @@ export const sendOtp = async (req, res) => {
 
   const otp = Math.floor(100000 + Math.random() * 900000);
 
-  try{
-    const newOtp = await otpModel.create({ email, otp });
-    res.status(201).json({ message: "OTP sent successfully" });
-
-  }  catch(error){
-    res.status(500).json({ message: error.message });
-  }
+  const newOtp = await otpModel.create({ email, otp });
 
  
 
